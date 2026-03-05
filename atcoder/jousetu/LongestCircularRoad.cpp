da9eda18af684ffd1f5045b7f348859e1308e480
oriri-byte <179485773+oriri-byte@users.noreply.github.com>
2026-03-06 00:03:28 +0900

    グラフの直径 use bfs.

--- atcoder/jousetu/LongestCircularRoad.cpp
+++ atcoder/jousetu/LongestCircularRoad.cpp
@@ -1,35 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-pair<int, int> dfs(int node, int parent, vector<vector<int>> &roads)
+int N;
+vector<vector<int>> roads;
+
+vector<int> search_farthest(int node)
 {
-    pair<int, int> ret = {0, node};
-    for (int &e : roads[node])
+  vector<int> dist(N, -1);
+  queue<int> que;
+  dist[node] = 0;
+  que.push(node);
+
+  while (!que.empty())
+  {
+    int v = que.front();
+    que.pop();
+    // cout << "current_node = " << v << endl;
+    for (auto nv : roads[v])
     {
-        if (e == parent)
-            continue;
-        pair<int, int> tmp = dfs(e, node, roads);
-        tmp.first++;
-        ret = max(ret, tmp);
+      if (dist[nv] != -1)
+        continue;
+      dist[nv] = dist[v] + 1;
+      que.push(nv);
     }
-    return move(ret);
+  }
+  return dist;
 }
+
 int main()
 {
-    int N;
-    int A, B;
-    cin >> N;
-    vector<vector<int>> roads(N + 1);
-    vector<int> depth(N + 1, -1);
-    for (int i = 0; i < N - 1; i++)
-    {
-        cin >> A >> B;
-        roads[A].push_back(B);
-        roads[B].push_back(A);
-    }
-    pair<int, int> start = dfs(1, 0, roads);
-    pair<int, int> diameter = dfs(start.second, 0, roads);
-    cout << diameter.first + 1;
+  cin >> N;
+  roads.resize(N);
 
-    return 0;
+  for (int i = 0; i < N - 1; i++)
+  {
+    int a, b;
+    cin >> a >> b;
+    a--;
+    b--;
+    roads[a].push_back(b);
+    roads[b].push_back(a);
+  }
+  vector<int> dist = search_farthest(0);
+  int idx = distance(dist.begin(), max_element(dist.begin(), dist.end()));
+  vector<int> fin_dist = search_farthest(idx);
+  cout << *max_element(fin_dist.begin(), fin_dist.end()) + 1 << endl;
+  return 0;
 }
