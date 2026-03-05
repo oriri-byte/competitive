70f9d182c906786d7c3b300956a5284c05ff25d8
oriri-byte <179485773+oriri-byte@users.noreply.github.com>
2026-03-05 17:28:38 +0900

    use dfs

--- atcoder/jousetu/EncyclopediaOfParentheses.cpp
+++ atcoder/jousetu/EncyclopediaOfParentheses.cpp
@@ -1,30 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void dfs(string s, int &N, int left, int right)
+void dfs(string s, int left, int right, int &N)
 {
     if (left + right == N)
     {
         cout << s << endl;
+        return;
     }
-    if (left < N / 2)
-    {
-        dfs(s + "(", N, left + 1, right);
-    }
-    if (right < left)
-    {
-        dfs(s + ")", N, left, right + 1);
-    }
+    if (left >= right && left < N / 2)
+        dfs(s + "(", left + 1, right, N);
+    if (left > right)
+        dfs(s + ")", left, right + 1, N);
 }
 
 int main()
 {
     int N;
     cin >> N;
-    if (N % 2 == 1)
-    {
+    if (N % 2)
         return 0;
-    }
-    dfs("", N, 0, 0);
+    dfs("", 0, 0, N);
     return 0;
 }
