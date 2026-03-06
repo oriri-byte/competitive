f046328db1b2d137d816605071e1898c5de36509
oriri-byte <179485773+oriri-byte@users.noreply.github.com>
2026-03-07 02:40:52 +0900

    use lower_bound and sort.

--- atcoder/jousetu/CPClasses.cpp
+++ atcoder/jousetu/CPClasses.cpp
@@ -3,31 +3,25 @@ using namespace std;
 
 int main()
 {
-    int N, Q;
+    int N, Q, B;
     cin >> N;
-    vector<long long> A(N);
+    vector<int> class_rate(N);
     for (int i = 0; i < N; i++)
-    {
-        cin >> A[i];
-    }
-    sort(A.begin(), A.end());
+        cin >> class_rate[i];
+    sort(class_rate.begin(), class_rate.end());
     cin >> Q;
-    long long B;
     for (int i = 0; i < Q; i++)
     {
         cin >> B;
-        auto itr = lower_bound(A.begin(), A.end(), B);
-        int idx = distance(A.begin(), itr);
-        long long ans;
+        auto itr = lower_bound(class_rate.begin(), class_rate.end(), B);
+        int idx = distance(class_rate.begin(), itr);
+        int ans;
         if (idx == 0)
-        {
-            ans = abs(*itr - B);
-        }
+            ans = abs(B - class_rate[0]);
+        else if (idx == N)
+            ans = abs(B - class_rate[N - 1]);
         else
-        {
-            ans = min(abs(*itr - B), abs(*(itr - 1) - B));
-        }
-
+            ans = min(abs(*itr - B), abs(*(itr--) - B));
         cout << ans << endl;
     }
     return 0;
